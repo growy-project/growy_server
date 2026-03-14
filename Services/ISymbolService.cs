8d7bac8c895ffcc73af2c80c1da0b9d28fa87672
dario.olinuk <dario.olinuk@quisitive.com>
2026-03-14 15:42:37 -0300

    Start Date and End date validations added

--- Services/ISymbolService.cs
+++ Services/ISymbolService.cs
@@ -1,8 +1,11 @@
+using growy_server.Models;
+
 namespace growy_server.Services
 {
     public interface ISymbolService
     {
         Task SetSymbolAsTopGrowth(string symbol, bool value);
         Task SetSymbolAsToxic(string symbol, bool value);
+        Task<SymbolDateRangeResult> GetDateRange(string exchange);
     }
 }
